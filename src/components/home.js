365b897aca471301d5dbefd0306df856d279707a
Sonakshi1901 <sonakshi1901@gmail.com>
2021-04-30 22:40:16 +0400

    home

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./home.css";
+import Header from "./header.js";
+
+function Home() {
+    return (
+      <section>
+        <div class="container">
+          <div class="left"></div>
+          <div class="right">
+            <div class="content">
+              <h1>This is a heading</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda dolore reprehenderit ullam blanditiis ex sapiente pariatur tempore incidunt facilis?</p>
+              <button onClick="">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Home
