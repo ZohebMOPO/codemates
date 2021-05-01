cd3d82dcfaa67e7f4a5919edfc656aa3e32fe188
Sonakshi1901 <sonakshi1901@gmail.com>
2021-05-01 12:24:29 +0400

    changes

--- src/wrapper.js
+++ src/wrapper.js
@@ -6,13 +6,13 @@ import Options from './components/options'
 function Wrapper() {
     return (
         <div>
+            <Header />
             <div>
               <Cards
                name="ALENA STUDIO"
                profession= "WEB DEVELOPER"
                img="https://en.gravatar.com/userimage/42992842/b8e8ff24d6f1cb84bbe5c839903086bb.png?size=200"/>
             </div>
-            <Header />
             <Options />
         </div>
     )
