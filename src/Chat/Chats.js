6295c044e87ca23ec7b6177f2d44d42ea2e4c382
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-02 11:54:54 +0530

    Chat Feature

--- src/Chat/Chats.js
+++ src/Chat/Chats.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Chats.css'
+import Chat from './Chat'
+import React from 'react';
+import './Chats.css';
 
 function Chats() {
+    
     return (
         <div>
-            
+            <div className='chats'>
+                <Chat
+                 name=''
+                 message=''
+                 timestamp=''
+                 profilePic=''
+                 />
+            </div>
         </div>
     )
 }
